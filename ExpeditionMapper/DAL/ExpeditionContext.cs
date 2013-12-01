ae4163d0e9a57e0af454e3264e5a18eb7329d0a2
Ian Carnaghan <icarnaghan@gmail.com>
2013-11-30 21:39:34 -0500

    Adding context.

---
+++ ExpeditionMapper/DAL/ExpeditionContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using ExpeditionMapper.Models.Domain;
+
+namespace ExpeditionMapper.DAL
+{
+	public class ExpeditionContext : DbContext
+	{
+        public ExpeditionContext(): base("ExpeditionContext")
+	    {    
+	    }
+
+	    public DbSet<Program> Programs { get; set; }
+	    public DbSet<FswSemester> FswSemesters { get; set; }
+	    public DbSet<FallExpedition> FallExpeditions { get; set; }
+	    public DbSet<MiniSemester> MiniSemesters { get; set; }
+	    public DbSet<SpringExpedition> SpringExpeditions { get; set; }
+	    public DbSet<GuidingQuestion> GuidingQuestions { get; set; }
+	    public DbSet<FinalProduct> FinalProducts { get; set; }
+	}
+}
---
+++ ExpeditionMapper/Models/Domain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ExpeditionMapper.Models.Domain
+{
+    public abstract class Program
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
