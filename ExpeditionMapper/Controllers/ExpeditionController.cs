e18f3f1ea9bd0458684996930f8efbbfe56ca16c
Ian Carnaghan <icarnaghan@gmail.com>
2013-12-07 08:05:06 -0500

    Adding additional Flat UI files and new expedition controller.

---
+++ ExpeditionMapper/Controllers/ExpeditionController.cs
@@ -0,0 +1,171 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using ExpeditionMapper.Models.Domain;
+using ExpeditionMapper.DAL;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+
+namespace ExpeditionMapper.Controllers
+{
+    public class ExpeditionController : Controller
+    {
+        private ExpeditionContext db = new ExpeditionContext();
+
+        // GET: /Expedition/
+        public ActionResult Index()
+        {
+            var expeditions = db.Expeditions.Include(e => e.GradeLevel).Include(e => e.GuidingQuestions);
+            return View(expeditions.ToList());
+        }
+
+        public ActionResult GuidingQuestions_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            IQueryable<GuidingQuestion> guidingQuestions = db.GuidingQuestions;
+            return Json(guidingQuestions.ToDataSourceResult(request));
+        }
+
+        public ActionResult New()
+        {
+            var expedition = new Expedition();
+            expedition.GradeLevelId = 2;
+            expedition.CreateGuidingQuestions(2);
+            return View(expedition);
+        }
+
+        [HttpPost]
+        public ActionResult New(Expedition expedition)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (GuidingQuestion guidingQuestion in expedition.GuidingQuestions.ToList())
+                {
+                    if (guidingQuestion.DeleteGuidingQuestion == true)
+                    {
+                        // Delete Phone Numbers which is marked to remove
+                        expedition.GuidingQuestions.Remove(guidingQuestion);
+                    }
+                }
+                db.Expeditions.Add(expedition);
+                db.SaveChanges();
+            }
+            return Redirect("New");
+        }
+
+        // GET: /Expedition/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expedition expedition = db.Expeditions.Find(id);
+            if (expedition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expedition);
+        }
+
+        // GET: /Expedition/Create
+        public ActionResult Create()
+        {
+            ViewBag.GradeLevelId = new SelectList(db.GradeLevels, "Id", "Name");
+            return View();
+        }
+
+        // POST: /Expedition/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,Year,GradeLevelId,Name,Description,KickOff,FinalProductName,FinalProductDescription,GuidingQuestions")] Expedition expedition)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Expeditions.Add(expedition);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GradeLevelId = new SelectList(db.GradeLevels, "Id", "Name", expedition.GradeLevelId);
+            return View(expedition);
+        }
+
+        // GET: /Expedition/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expedition expedition = db.Expeditions.Find(id);
+            if (expedition == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.GradeLevelId = new SelectList(db.GradeLevels, "Id", "Name", expedition.GradeLevelId);
+            return View(expedition);
+        }
+
+        // POST: /Expedition/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,Year,GradeLevelId,Name,Description,KickOff,FinalProductName,FinalProductDescription,GuidingQuestions")] Expedition expedition)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(expedition).State = EntityState.Modified;
+                db.SaveChanges();
+                
+                foreach (var item in expedition.GuidingQuestions)
+                {
+                    db.Entry(item).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                
+                return RedirectToAction("Index");
+            }
+            ViewBag.GradeLevelId = new SelectList(db.GradeLevels, "Id", "Name", expedition.GradeLevelId);
+            return View(expedition);
+        }
+
+        // GET: /Expedition/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expedition expedition = db.Expeditions.Find(id);
+            if (expedition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expedition);
+        }
+
+        // POST: /Expedition/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Expedition expedition = db.Expeditions.Find(id);
+            db.Expeditions.Remove(expedition);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ExpeditionMapper/Models/Domain/Expedition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using ExpeditionMapper.Models.Domain.LookUps;
+
+namespace ExpeditionMapper.Models.Domain
+{
+    public class Expedition
+    {
+        public Expedition()
+        {
+            this.GuidingQuestions = new HashSet<GuidingQuestion>();
+        }
+
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public int GradeLevelId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string KickOff { get; set; }
+        public string FinalProductName { get; set; }
+        public string FinalProductDescription { get; set; }
+
+        [ForeignKey("GradeLevelId")]
+        public virtual GradeLevel GradeLevel { get; set; }
+
+        public virtual ICollection<GuidingQuestion> GuidingQuestions { get; set; }
+
+        internal void CreateGuidingQuestions(int count = 1)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                GuidingQuestions.Add(new GuidingQuestion());
+            }
+        }
+    }
+}
