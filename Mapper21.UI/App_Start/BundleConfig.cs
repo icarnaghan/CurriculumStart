d84a05cb65f648dbf52a43e02770f6c397c3cdd0
Ian Carnaghan <ian@carnaghan.com>
2014-08-07 16:40:11 -0400

    Cleanup
    Removing / reorganizing third party libraries

--- Mapper21.UI/App_Start/BundleConfig.cs
+++ Mapper21.UI/App_Start/BundleConfig.cs
@@ -24,37 +24,37 @@ namespace Mapper21.UI
                 ));
 
             bundles.Add(new ScriptBundle("~/bundles/AdminLTE").Include(
-                "~/Scripts/AdminLTE/jquery-ui-1.10.3.min.js",
-                "~/Scripts/AdminLTE/plugins/morris/morris.min.js",
-                "~/Scripts/AdminLTE/plugins/sparkline/jquery.sparkline.min.js",
-                "~/Scripts/AdminLTE/plugins/fullcalendar/fullcalendar.min.js",
-                "~/Scripts/AdminLTE/plugins/jqueryKnob/jquery.knob.js",
-                "~/Scripts/AdminLTE/plugins/daterangepicker/daterangepicker.js",
-                "~/Scripts/AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js",
-                "~/Scripts/AdminLTE/plugins/iCheck/icheck.min.js",
-                "~/Scripts/AdminLTE/AdminLTE/app.js"
+                "~/Scripts/Libraries/AdminLTE/jquery-ui-1.10.3.min.js",
+                "~/Scripts/Libraries/AdminLTE/plugins/morris/morris.min.js",
+                "~/Scripts/Libraries/AdminLTE/plugins/sparkline/jquery.sparkline.min.js",
+                "~/Scripts/Libraries/AdminLTE/plugins/fullcalendar/fullcalendar.min.js",
+                "~/Scripts/Libraries/AdminLTE/plugins/jqueryKnob/jquery.knob.js",
+                "~/Scripts/Libraries/AdminLTE/plugins/daterangepicker/daterangepicker.js",
+                "~/Scripts/Libraries/AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js",
+                "~/Scripts/Libraries/AdminLTE/plugins/iCheck/icheck.min.js",
+                "~/Scripts/Libraries/AdminLTE/AdminLTE/app.js"
                 ));
 
             bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
-                "~/Scripts/kendo/2014.1.318/kendo.all.min.js",
-                "~/Scripts/kendo/2014.1.318/kendo.aspnetmvc.min.js"));
+                "~/Scripts/Libraries/KendoUI/kendo.all.min.js",
+                "~/Scripts/Libraries/KendoUI/kendo.aspnetmvc.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
+                "~/Content/Libraries/bootstrap.css",
                 "~/Content/site.css",
-                "~/Content/AdminLTE/css/font-awesome.min.css",
-                "~/Content/AdminLTE/css/ionicons.min.css",
-                "~/Content/AdminLTE/css/morris/morris.css",
-                "~/Content/AdminLTE/css/jvectormap/jquery-jvectormap-1.2.2.css",
-                "~/Content/AdminLTE/css/fullcalendar/fullcalendar.css",
-                "~/Content/AdminLTE/css/daterangepicker/daterangepicker-bs3.css",
-                "~/Content/AdminLTE/css/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css",
-                "~/Content/AdminLTE/css/AdminLTE.css"
+                "~/Content/Libraries/AdminLTE/css/font-awesome.min.css",
+                "~/Content/Libraries/AdminLTE/css/ionicons.min.css",
+                "~/Content/Libraries/AdminLTE/css/morris/morris.css",
+                "~/Content/Libraries/AdminLTE/css/jvectormap/jquery-jvectormap-1.2.2.css",
+                "~/Content/Libraries/AdminLTE/css/fullcalendar/fullcalendar.css",
+                "~/Content/Libraries/AdminLTE/css/daterangepicker/daterangepicker-bs3.css",
+                "~/Content/Libraries/AdminLTE/css/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css",
+                "~/Content/Libraries/AdminLTE/css/AdminLTE.css"
                 ));
 
             bundles.Add(new StyleBundle("~/Content/kendo/css").Include(
-                "~/Content/kendo/2014.1.318/kendo.common.min.css",
-                "~/Content/kendo/2014.1.318/kendo.bootstrap.min.css"));
+                "~/Content/Libraries/KendoUI/kendo.common.min.css",
+                "~/Content/Libraries/KendoUI/kendo.bootstrap.min.css"));
 
             // Allow minified files in debug mode.
             bundles.IgnoreList.Clear();
