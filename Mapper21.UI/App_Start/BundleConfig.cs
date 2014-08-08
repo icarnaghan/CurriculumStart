641d16f11f4a3dc84d50650326bce537a151e210
Ian Carnaghan <ian@carnaghan.com>
2014-08-07 22:52:46 -0400

    Bugfixes and refactoring

--- Mapper21.UI/App_Start/BundleConfig.cs
+++ Mapper21.UI/App_Start/BundleConfig.cs
@@ -8,7 +8,8 @@ namespace Mapper21.UI
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+                //"~/Scripts/jquery-{version}.js"));
+                "~/Scripts/Libraries/KendoUI/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.validate*"));
---
+++ Mapper21.UI/Controllers/FileBrowserController.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using Kendo.Mvc.UI;
+
+namespace Mapper21.UI.Controllers
+{
+    public class FileBrowserController : EditorFileBrowserController
+    {
+        private const string contentFolderRoot = "~/Content/";
+        private const string prettyName = "Images/";
+        private static readonly string[] foldersToCopy = new[] { "~/Content/shared/" };
+
+
+        /// <summary>
+        /// Gets the base paths from which content will be served.
+        /// </summary>
+        public override string ContentPath
+        {
+            get
+            {
+                return CreateUserFolder();
+            }
+        }
+
+        /// <summary>
+        /// Gets the valid file extensions by which served files will be filtered.
+        /// </summary>
+        public override string Filter
+        {
+            get
+            {
+                return "*.txt, *.doc, *.docx, *.xls, *.xlsx, *.ppt, *.pptx, *.zip, *.rar, *.jpg, *.jpeg, *.gif, *.png";
+            }
+        }
+
+        private string CreateUserFolder()
+        {
+            var virtualPath = Path.Combine(contentFolderRoot, "UserFiles", prettyName);
+
+            var path = Server.MapPath(virtualPath);
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+                foreach (var sourceFolder in foldersToCopy)
+                {
+                    CopyFolder(Server.MapPath(sourceFolder), path);
+                }
+            }
+            return virtualPath;
+        }
+
+        private void CopyFolder(string source, string destination)
+        {
+            if (!Directory.Exists(destination))
+            {
+                Directory.CreateDirectory(destination);
+            }
+
+            foreach (var file in Directory.EnumerateFiles(source))
+            {
+                var dest = Path.Combine(destination, Path.GetFileName(file));
+                System.IO.File.Copy(file, dest);
+            }
+
+            foreach (var folder in Directory.EnumerateDirectories(source))
+            {
+                var dest = Path.Combine(destination, Path.GetFileName(folder));
+                CopyFolder(folder, dest);
+            }
+        }
+    }
+}
