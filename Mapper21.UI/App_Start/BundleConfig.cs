28b78b6ccbcaa817059956e2c60e2616852f7cf4
Ian Carnaghan <ian@carnaghan.com>
2014-08-07 23:25:40 -0400

    Added file and image upload features, upgraded library files

--- Mapper21.UI/App_Start/BundleConfig.cs
+++ Mapper21.UI/App_Start/BundleConfig.cs
@@ -8,8 +8,7 @@ namespace Mapper21.UI
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                //"~/Scripts/jquery-{version}.js"));
-                "~/Scripts/Libraries/KendoUI/jquery.min.js"));
+                "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.validate*"));
