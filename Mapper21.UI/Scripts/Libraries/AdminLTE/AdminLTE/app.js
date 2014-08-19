ebfdebbd52573b8412b1bb43dec608930287b341
Ian Carnaghan <ian@carnaghan.com>
2014-08-19 09:28:16 -0400

    Removing some demo / test code

--- Mapper21.UI/Scripts/Libraries/AdminLTE/AdminLTE/app.js
+++ Mapper21.UI/Scripts/Libraries/AdminLTE/AdminLTE/app.js
@@ -135,18 +135,18 @@ $(function() {
     });
 
     /* For demo purposes */
-    var demo = $("<div />").css({
-        position: "fixed",
-        top: "150px",
-        right: "0",
-        background: "rgba(0, 0, 0, 0.7)",
-        "border-radius": "5px 0px 0px 5px",
-        padding: "10px 15px",
-        "font-size": "16px",
-        "z-index": "999999",
-        cursor: "pointer",
-        color: "#ddd"
-    }).html("<i class='fa fa-gear'></i>").addClass("no-print");
+    //var demo = $("<div />").css({
+    //    position: "fixed",
+    //    top: "150px",
+    //    right: "0",
+    //    background: "rgba(0, 0, 0, 0.7)",
+    //    "border-radius": "5px 0px 0px 5px",
+    //    padding: "10px 15px",
+    //    "font-size": "16px",
+    //    "z-index": "999999",
+    //    cursor: "pointer",
+    //    color: "#ddd"
+    //}).html("<i class='fa fa-gear'></i>").addClass("no-print");
 
     var demo_settings = $("<div />").css({
         "padding": "10px",
