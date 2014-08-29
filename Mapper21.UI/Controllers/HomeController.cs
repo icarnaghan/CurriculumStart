16d71173776484e51834be290b8bd548708d06f9
Ian Carnaghan <ian@carnaghan.com>
2014-08-29 18:34:27 -0400

    Temporarily fixed session bug

--- Mapper21.UI/Controllers/HomeController.cs
+++ Mapper21.UI/Controllers/HomeController.cs
@@ -8,6 +8,11 @@ namespace Mapper21.UI.Controllers
     {
         public ActionResult Index()
         {
+            // TODO - Refactor this - shouldn't need to set this at login and here
+            if (Session != null && Session["GradeLevel"] == null)
+            {
+                Session["GradeLevel"] = "K";
+            }
             var year = Int32.Parse(CurrentYear);
             if (Session != null) ViewBag.Grade = CurrentGradeLevel == "" ? Session["GradeLevel"] : CurrentGradeLevel;
             ViewBag.Year = year  + " - " + (year + 1);
