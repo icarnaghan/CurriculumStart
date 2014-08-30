9e54592600bea4cb8ef3d19f5aa6a8d3e70ce976
Ian Carnaghan <ian@carnaghan.com>
2014-08-30 10:38:52 -0400

    Unversioning directories

--- Mapper21.Site/Properties/AssemblyInfo.cs
+++ Mapper21.Site/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Mapper21")]
+[assembly: AssemblyTitle("Mapper21.Site")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mapper21")]
+[assembly: AssemblyProduct("Mapper21.Site")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
