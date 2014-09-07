86d8dcd00cc3fb019298b84dd215d9d1a64277e0
Ian Carnaghan <ian@carnaghan.com>
2014-09-07 14:47:40 -0400

    Purging unneeded files

--- Mapper21.Business/Dto/LookUps/GradeLevelLookupDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mapper21.Business.Dto.LookUps
-{
-    public class GradeLevelLookupDto
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- Mapper21.Business/Dto/LookUps/Semester.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ExpeditionMapper.BE.Domain.LookUps
-{
-    public class Semester
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
