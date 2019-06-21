76d9d73da4da5269d8bd6c101207aea4366b82ba
Ian Carnaghan <carnaghan_ian@bah.com>
2019-06-21 12:27:00 -0400

    Added and integrated alertify js as a service

--- CurriculumStart-SPA/src/app/app.module.ts
+++ CurriculumStart-SPA/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
 
 @NgModule({
    declarations: [
@@ -26,7 +27,8 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
    ],
    providers: [
       AuthService,
-      ErrorInterceptorProvider
+      ErrorInterceptorProvider,
+      AlertifyService
    ],
    bootstrap: [
       AppComponent
