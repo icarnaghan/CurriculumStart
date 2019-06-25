5e65f18539fa14830079d14d2853559f72d807b9
Ian Carnaghan <carnaghan_ian@bah.com>
2019-06-24 21:42:21 -0400

    Added ngx-bootstrap

--- CurriculumStart-SPA/src/app/app.module.ts
+++ CurriculumStart-SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +24,8 @@ import { AlertifyService } from './_services/alertify.service';
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
