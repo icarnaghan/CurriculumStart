8ef61fa2111b34dffbe13be337edbc3b2f765e91
Ian Carnaghan <carnaghan_ian@bah.com>
2019-06-28 09:14:31 -0400

    Added persistence of data for user form

---
+++ CurriculumStart.API/Dtos/UserForUpdateDto.cs
@@ -0,0 +1,11 @@
+namespace CurriculumStart.API.Dtos
+{
+    public class UserForUpdateDto
+    {
+        public string Introduction { get; set; }
+        public string Expertise { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
--- CurriculumStart.API/Helpers/AutoMapperProfiles.cs
+++ CurriculumStart.API/Helpers/AutoMapperProfiles.cs
@@ -24,6 +24,7 @@ namespace CurriculumStart.API.Helpers
                     opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
                 });
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
