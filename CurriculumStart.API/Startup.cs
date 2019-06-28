2677639f5acb2e56d82ec4db938d328f0454df10
Ian Carnaghan <carnaghan_ian@bah.com>
2019-06-28 09:50:22 -0400

    Added Cloudinary and updated photo model & migrations

--- CurriculumStart.API/Startup.cs
+++ CurriculumStart.API/Startup.cs
@@ -42,6 +42,7 @@ namespace CurriculumStart.API
                         Newtonsoft.Json.ReferenceLoopHandling.Ignore;
                 });
             services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddAutoMapper();
             services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
