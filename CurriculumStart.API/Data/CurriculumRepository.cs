ea2b8bdba0993ba2fc7c1fc340878ec57f2db1b5
Ian Carnaghan <carnaghan_ian@bah.com>
2019-07-04 21:56:44 -0400

    Implemented some basic filtering in the API

--- CurriculumStart.API/Data/CurriculumRepository.cs
+++ CurriculumStart.API/Data/CurriculumRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -46,7 +47,19 @@ namespace CurriculumStart.API.Data
 
         public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users = _context.Users.Include(p => p.Photos);
+            var users = _context.Users.Include(p => p.Photos).AsQueryable();
+
+            users = users.Where(u => u.Id != userParams.UserId);
+
+            users = users.Where(u => u.Gender == userParams.Gender);
+
+            if (userParams.MinAge != 18 || userParams.MaxAge != 99)
+            {
+                var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+                var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+
+                users = users.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+            }
 
             return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
--- CurriculumStart.API/Helpers/UserParams.cs
+++ CurriculumStart.API/Helpers/UserParams.cs
@@ -10,6 +10,9 @@ namespace CurriculumStart.API.Helpers
             get { return pageSize; }
             set { pageSize = (value > MaxPageSize) ? MaxPageSize : value ;}
         }
-        
+        public int UserId { get; set; }
+        public string Gender { get; set; }
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 99;
     }
 }
