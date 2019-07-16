47df75c76c6679227b2585677bbe9227496c7c50
Ian Carnaghan <ian@carnaghan.com>
2019-07-16 06:55:23 -0400

    Adding basic roles to seed method

--- CurriculumStart.API/Data/Seed.cs
+++ CurriculumStart.API/Data/Seed.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using CurriculumStart.API.Models;
 using Microsoft.AspNetCore.Identity;
@@ -8,8 +9,10 @@ namespace CurriculumStart.API.Data
     public class Seed
     {
         private readonly UserManager<User> _userManager;
-        public Seed(UserManager<User> userManager)
+        private readonly RoleManager<Role> _roleManager;
+        public Seed(UserManager<User> userManager, RoleManager<Role> RoleManager)
         {
+            _roleManager = RoleManager;
             _userManager = userManager;
         }
 
@@ -19,9 +22,37 @@ namespace CurriculumStart.API.Data
             {
                 var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
                 var users = JsonConvert.DeserializeObject<System.Collections.Generic.List<User>>(userData);
+                
+                var roles = new List<Role>
+                {
+                    new Role { Name = "Member" },
+                    new Role { Name = "Admin" },
+                    new Role { Name = "Moderator" },
+                    new Role { Name = "VIP" }
+                };
+
+                foreach (var role in roles)
+                {
+                    _roleManager.CreateAsync(role).Wait();
+                }
+                
                 foreach (var user in users)
                 {
                     _userManager.CreateAsync(user, "password").Wait();
+                    _userManager.AddToRoleAsync(user, "Member").Wait();
+                }
+
+                var adminUser = new User
+                {
+                    UserName = "Admin"
+                };
+
+                IdentityResult result = _userManager.CreateAsync(adminUser, "password").Result;
+
+                if (result.Succeeded)
+                {
+                    var admin = _userManager.FindByNameAsync("Admin").Result;
+                    _userManager.AddToRolesAsync(admin, new[] {"Admin", "Moderator"}).Wait();
                 }
             }
         }
